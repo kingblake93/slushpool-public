7ff2743ec944989e4dc0ecfd9670ae640e93d3b9
Blake King <49929719+kingblake93@users.noreply.github.com>
2021-07-20 20:10:54 -0500

    Adding some comments to main

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
-import random
 import time
 from utils import *
 
+#  This is an infinite loop, everything within while True will
+#  continue until the code crashes.
+
 while True:
     try:
+        # Get a string timestamp of RIGHT NOW in the format of
+        # %A, %d. %B %Y %I:%M%p.
         timestamp = datetime.now().strftime("%A, %d. %B %Y %I:%M%p")
         print(timestamp)
 
+        # Run update_log(), this will do all the heavy lifting
+        # of the script.
         all_blocks = update_log()
 
         # get_daily_btc(all_blocks)
 
+    # This is for catching any errors. This way, the code can
+    # still continue.
     except Exception as r:
         print(f'Exception {r} occurred.')
+
+    # After exiting the Try, Except clause, wait for 20 minutes
+    # and then start the infinite loop again
     time.sleep(1200)
 
